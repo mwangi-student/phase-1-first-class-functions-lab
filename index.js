542f4f8b579df0cda0dc0910cda02e81201264f1
chieby-mwangi <chiebyrah@gmail.com>
2024-09-29 13:21:41 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+// selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+
+// fareDoubler
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers
+const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
+};
